9500c9c7a7a7a2551892d10f4709113b2fbfc7e9
li shiqian <lishiqian@zhangyue.com>
2019-12-10 10:32:24 +0800

    Initial Commit

---
+++ Week/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Week
+//
+//  Created by Shiqian.Lee on 2019/12/10.
+//  Copyright © 2019 LeeShiqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Week/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Week
+//
+//  Created by Shiqian.Lee on 2019/12/10.
+//  Copyright © 2019 LeeShiqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
